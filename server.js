7bf99167c2d04d46b4a3cb9e1fc60bd51bebad15
Marco Mercado <marcore.dmg@gmail.com>
2021-08-16 17:04:13 -0500

    Part 1

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const serverless = require('serverless-http')
+
+let app = express()
+
+app.use(express.json())
+
+app.get('/', function(req, res) {
+    res.send('Hello world')
+})
+
+const PORT = 3000
+app.listen(PORT, function() {
+    console.log(`App listening on http://localhost:${PORT}`)
+})
+
+module.exports.handler = serverless(app)
